1a0b24bf7370b8b50de1ab708ee84550aec79566
Don Neill <neilljd@gmail.com>
2018-06-18 00:32:53 -0700

    added parse_imsec method to ccd_primatives.py

--- KCWIPyDRP/ccd_primitives.py
+++ KCWIPyDRP/ccd_primitives.py
@@ -29,3 +29,25 @@ class CcdPrimitives:
 
     def rectify_image(self):
         log.info("rectify_image")
+
+    def parse_imsec(self, section_key=None):
+        section = self.frame.header[section_key]
+        p1 = int(section[1:-1].split(',')[0].split(':')[0])
+        p2 = int(section[1:-1].split(',')[0].split(':')[1])
+        p3 = int(section[1:-1].split(',')[1].split(':')[0])
+        p4 = int(section[1:-1].split(',')[1].split(':')[1])
+        # tests
+        if p1 > p2:
+            x0 = p2
+            x1 = p1
+        else:
+            x0 = p1
+            x1 = p2
+        if p3 > p4:
+            y0 = p4
+            y1 = p3
+        else:
+            y0 = p3
+            y1 = p4
+        # use python axis ordering
+        return y0, y1, x0, x1
