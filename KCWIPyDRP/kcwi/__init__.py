6af12aca7bfda29ddf12a6364903181a5426fbca
Luca Rizzi <lrizzi@keck.hawaii.edu>
2018-06-21 12:40:07 -1000

    A working version of the config system

--- KCWIPyDRP/kcwi/__init__.py
+++ KCWIPyDRP/kcwi/__init__.py
@@ -2,3 +2,20 @@
 """
 This packages contains the KCWI specific primitives and recipes
 """
+# set up namespace, unless we are in setup...
+if not _ASTROPY_SETUP_:
+    # from .core import *
+    # from .ccddata import *
+    # from .combiner import *
+    # from .image_collection import *
+    from astropy import config as _config
+
+    class Conf(_config.ConfigNamespace):
+        """
+        Configuration parameters for KCWIPyDRP.
+        """
+        CRZAP= _config.ConfigItem(
+            True,
+            'Perform cosmic ray rejection'
+            )
+    conf = Conf()
