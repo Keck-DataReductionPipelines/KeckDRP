227b57c9e3ad102dbe1f9009d3fbe1b2ed3cb9d8
Luca Rizzi <luca.kilohi@gmail.com>
2019-02-13 09:17:05 -0800

    Adding IMTYPE selection to the looping modes

--- scripts/reduce.py
+++ scripts/reduce.py
@@ -23,27 +23,28 @@ parser.add_argument('--recipe', type=str, help='reduction recipe')
 parser.add_argument('--loop', action='store_true', help='Use infinite loop')
 parser.add_argument('--imlist',
                     help='File containing the frames to be reduced')
+parser.add_argument('--imtype', type=str, help='reduce all frames of the specified image type')
 parser.add_argument('frame', nargs='?', type=str, help='input image file')
 
 
-def main_loop(frame=None, recipe=None, loop=None, imlist=None):
+def main_loop(frame=None, recipe=None, loop=None, imlist=None, imtype=None):
     # case 1: one one image is specified
     if frame:
-        go(frame, recipe)
+        go(frame, recipe, imtype)
         return
     # case 2: infinite loop
     if loop:
         # step 1: build a list of files already in the directory
-        input_files_before = glob.glob('k*.fits')
+        input_files_before = glob.glob('*.fits')
         for input_file in input_files_before:
-            go(input_file, recipe)
+            go(input_file, recipe, imtype)
         while True:
             # step 2: start infinite loop waiting for new files
-            input_files_now = glob.glob('k*.fits')
+            input_files_now = glob.glob('*.fits')
             new_files = [f for f in input_files_now
                          if f not in input_files_before]
             for input_file in new_files:
-                go(input_file, recipe)
+                go(input_file, recipe, imtype)
             input_files_before = input_files_now
             time.sleep(10)
     # case 3: a list of files is provided
@@ -51,11 +52,11 @@ def main_loop(frame=None, recipe=None, loop=None, imlist=None):
         if os.path.isfile(imlist):
             with open(imlist) as file_list:
                 for file in file_list:
-                    go(file.rstrip('\n'), recipe)
+                    go(file.rstrip('\n'), recipe, imtype)
         return
 
 
-def go(image, rcp):
+def go(image, rcp, imtype=None):
 
     # load the frame and instantiate the object
     if os.path.isfile(image):
@@ -77,20 +78,26 @@ def go(image, rcp):
         Instrument = Instruments.KCWI()
 
     frame_type = Instrument.get_image_type(frame)
+    if imtype:
+        if imtype!=frame_type:
+            log.info("Frame %s (%s) is not of imtype %s and will be skipped" % (image, frame_type, imtype))
+            return
 
     recipe = Instrument.get_recipe(frame_type)
+    log.info("Frame %s is of IMTYPE %s and uses the recipe %s" % (image, frame_type, recipe))
+
     if recipe is None:
         log.info("\n--- No reduction necessary ---\n")
         return
 
-#    try:
-#        mymodule = importlib.import_module(f'KeckDRP.{inst}.recipes.{recipe}')
-    mymodule = importlib.import_module("KeckDRP.%s.recipes.%s" % (inst, recipe))
-    myrecipe = getattr(mymodule, recipe)
-#    except:
-#        log.warn(f'\n--- Recipe {recipe} does not exist')
-#            log.warn("\n--- Recipe %s does not exist" % (recipe))
-#            return
+    try:
+        mymodule = importlib.import_module("KeckDRP.%s.recipes.%s" % (inst, recipe))
+        myrecipe = getattr(mymodule, recipe)
+    except ImportError:
+        log.warn("\n--- Recipe %s does not exist" % (recipe))
+        return
+    except RuntimeError:
+        log.warn("\n--- Error looking for recipe %s" % (recipe))
 
     log.info("\n---  Reducing frame %s with recipe: %s ---" %
              (image, myrecipe.__name__))
@@ -117,12 +124,12 @@ if __name__ == '__main__':
 
     if args.frame:
         log.info("reducing image %s" % args.frame)
-        main_loop(frame=args.frame, recipe=args.recipe)
+        main_loop(frame=args.frame, recipe=args.recipe, imtype=args.imtype)
     elif args.loop:
         log.info("reducing in a loop")
-        main_loop(loop=args.loop, recipe=args.recipe)
+        main_loop(loop=args.loop, recipe=args.recipe, imtype=args.imtype)
     elif args.imlist:
         log.info("reducing images in list in %s" % args.imlist)
-        main_loop(imlist=args.imlist, recipe=args.recipe)
+        main_loop(imlist=args.imlist, recipe=args.recipe, imtype=args.imtype)
     else:
         log.info("Must supply an image or a list, or loop")
